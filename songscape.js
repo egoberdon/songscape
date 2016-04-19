d4a782bba9aa6239a4a3acbc5de1e7adb9472928
Ben Fallon <benjamin.a.fallon@gmail.com>
2016-04-19 19:48:57 -0400

    Add skybox

--- songscape.js
+++ songscape.js
@@ -86,6 +86,23 @@ function init()
 			zFacePosition -= 150; // go deeper
 		}
 	});
+	
+	//CODE FROM Stemkoski Skybox.html; TO-DO: customize with own sky images/textures
+	var imagePrefix = "images/space-";
+	var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
+		var imageSuffix = ".jpg";
+		var skyGeometry = new THREE.CubeGeometry( 5000, 5000, 5000 );	
+
+			var materialArray = [];
+	for (var i = 0; i < 6; i++)
+		materialArray.push( new THREE.MeshBasicMaterial({
+			map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+			side: THREE.BackSide
+		}));
+	var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+	var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	scene.add( skyBox );
+	
 }
 function animate()
 {
